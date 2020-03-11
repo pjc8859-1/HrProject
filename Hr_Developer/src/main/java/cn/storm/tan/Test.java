ce1b9bc4c32c3599135e6d3bc4694a903c6e81e2
77954 <77954@LAPTOP-CO8US9BN>
2020-03-11 16:28:02 +0800

    aa

--- Hr_Developer/src/main/java/cn/storm/tan/Test.java
+++ Hr_Developer/src/main/java/cn/storm/tan/Test.java
@@ -7,9 +7,11 @@ import org.apache.ibatis.transaction.Transaction;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import cn.storm.pojo.SalaryGrantDetails;
 import cn.storm.pojo.SalaryStandard;
 import cn.storm.pojo.SalaryStandardDetails;
 import cn.storm.pojo.Training;
+import cn.storm.service.SalaryGrantDetailsService;
 import cn.storm.service.SalaryStandardDetailsService;
 import cn.storm.service.SalaryStandardService;
 import cn.storm.service.TrainingService;
@@ -20,25 +22,20 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ApplicationContext ac = new ClassPathXmlApplicationContext("/applicationContext.xml");
-		SalaryStandardService service=   (SalaryStandardService) ac.getBean("salaryStandardServiceImpl");
-		SalaryStandard sala=new SalaryStandard();
-		sala.setChanger("1");
-		sala.setChangeStatus((short)2);
-		sala.setChangeTime(new Timestamp(new Date().getTime()));
-		sala.setDesigner("1");
-		sala.setRegister("a");
-		sala.setRegistTime(new Timestamp(new Date().getTime()));
-		sala.setRemark("c");
-		sala.setSalarySum(10.0);
-		sala.setSsdId((short)1);
-		sala.setStandardId("asd");
-		sala.setStandardName("asd");
+		SalaryGrantDetailsService service=    (SalaryGrantDetailsService) ac.getBean("salaryGrantDetailsServiceImpl");
+	   
+		SalaryGrantDetails s=new SalaryGrantDetails();
+		s.setBounsSum(200.0);
+		s.setDeductSum(300.0);
+		s.setGrdId((short) 1);
+		s.setHumanId("a");
+		s.setHumanName("3");
+		s.setSalaryGrantId("a");
+		s.setSalaryPaidSum(500.0);
+		s.setSalaryStandardSum(800.0);
+		s.setSaleSum(1000.0);
 		
-		sala.setChecker("aa");
-		sala.setCheckComment("aaa");
-		sala.setCheckStatus((short)4);
-		
-		System.out.println(service.removeSalaryStandardBySsd(1));
+		System.out.println(service.removeSalaryGrantDetailsByGrdId(1));
 		
 		
 
