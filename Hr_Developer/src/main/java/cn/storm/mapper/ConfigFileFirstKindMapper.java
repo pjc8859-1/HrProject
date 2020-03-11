97cc502fcb66d20439664d0347b29b9ba178d332
maoyange9804 <1329016283@qq.com>
2020-03-11 23:54:35 +0800

    两张表

---
+++ Hr_Developer/src/main/java/cn/storm/mapper/ConfigFileFirstKindMapper.java
@@ -0,0 +1,16 @@
+package cn.storm.mapper;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import cn.storm.pojo.ConfigFileFirstKind;
+
+@Repository
+public interface ConfigFileFirstKindMapper {
+	public boolean saveConfigFileFirstKind(ConfigFileFirstKind configFileFirstKind);
+	public List<ConfigFileFirstKind> selectAllConfigFileFirstKind();
+	public ConfigFileFirstKind selectConfigFileFirstKindByffk_id(int sid);
+	public boolean deleteConfigFileFirstKindByffk_id(int sid);
+	public int updateConfigFileFirstKind(ConfigFileFirstKind configFileFirstKind);
+}
